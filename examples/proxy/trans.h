d9623ef8ac6701c329424d8a4d255b7dcbaca872
expost <tpxkldh@163.com>
2020-12-29 21:18:40 +0800

    test according libco

--- examples/proxy/trans.h
+++ examples/proxy/trans.h
@@ -14,7 +14,6 @@
 
 void print(...)
 {
-
 }
 class Trans
 {
@@ -60,115 +59,20 @@ public:
 
     int co_read(int socks, uint8_t *buf, size_t read_count, bool over = false)
     {
-        
-        int readed = 0;
-        int last_read = read_count;
-        int n = 0;
-        while (true)
-        {
-            n = read(socks, &buf[readed], last_read);
+        modify_mod(socks, this, EPOLLIN);
+        co_ptr_->yield(0); // 退出
 
-            if (n == 0)
-            {
-                // 关闭了连接，这里不处理
-                printf("n is zeor\n");
-                break;
-            }
-            else if (n < 0)
-            {
-                if (n == -1 && errno == EAGAIN)
-                {
-                    // test[this] += 1;
-                    // if(test[this] > 10)
-                    // {
-                    //     exit(-1);
-                    // }
-                    modify_mod(socks, this, EPOLLIN);
-                    printf("[%p] read error n is %d, %d:%d\n", this, socks, n, errno);
-                    co_ptr_->yield(0); // 退出
-                    continue;
-                }
-            
-                break;
-            }
-            else
-            {
-                readed += n;
-                last_read -= n;
-            }
-
-            if (over)
-            {
-                //printf("[%p] read count %d, last_read %d\n", this, readed, last_read);
-                return readed;
-            }
-
-            if (readed < read_count)
-            {
-                 modify_mod(socks, this, EPOLLIN);
-                co_ptr_->yield(0); // 退出
-                continue;
-            }
-            else
-            {
-                return readed;
-            }
-        };
-
+        int n = read(socks, buf, read_count);
         return n;
     }
 
     int co_send(int socks, uint8_t *buf, size_t write_count)
     //int co_send(int socks, void *trans, uint8_t *buf, size_t write_count)
     {
-        //test[this] = 0;
-        int writed = 0;
-        int last_write = write_count;
-        int n = 0;
-        while (1)
-        {
-            //printf("[%p] buf:%p, write_count:%u\n", this, buf, write_count);
-            int n = write(socks, buf, write_count);
-            if (n == 0)
-            {
-                // 链接关闭了，返回由上层处理
-                printf("write n is zero\n");
-                break;
-            }
-            if (n < 0)
-            {
-                //EAGAIN;
-                if (n == -1 && errno == EAGAIN)
-                {
-                    // modify_mod(socks, this, EPOLLOUT);
-                    // printf("[%p] write n is %d:%d\n", this, n, errno);
-                    // co_ptr_->yield(0); // 退出
-                    printf("[%p] write n is %d:%d\n", this, n, errno);
-                    continue;
-                }
-
-                break;
-            }
-            else
-            {
-                writed += n;
-                last_write -= n;
-            }
-
-            if (writed < write_count)
-            {
-                printf("little %d:%d\n", writed, write_count);
-                // modify_mod(socks, this, EPOLLOUT);
-                // co_ptr_->yield(0); // 退出
-                continue;
-            }
-            else
-            {
-                //printf("real write %d\n", writed);
-                
-                return writed;
-            }
-        };
+        modify_mod(socks, this, EPOLLOUT);
+        
+        co_ptr_->yield(0); // 退出
+        int n = write(socks, buf, write_count);
 
         return n;
     }
